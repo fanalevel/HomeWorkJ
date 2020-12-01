594dbadbf092b8b86d2bf696e27ea3820aba6f32
Антон Фролов <fanlion@gmail.com>
2020-12-01 16:59:02 +0200

    home work 2 first stage

---
+++ hw2/index.js
@@ -0,0 +1,14 @@
+const citiesAndCountries = {
+	'Киев': 'Украина',
+	'Нью-Йорк': 'США',
+	'Амстердам': 'Нидерланды',
+	'Берлин': 'Германия',
+	'Париж': 'Франция',
+	'Лиссабон': 'Португалия',
+	'Вена': 'Австрия',
+};
+
+for (let key in citiesAndCountries) {
+      console.log(key + ' это ' + citiesAndCountries[key]);
+}
+
