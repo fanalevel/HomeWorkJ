3fa01cff774d0707f32d829bb52938ed4f278341
Антон Фролов <fanlion@gmail.com>
2020-12-11 18:16:23 +0200

    task 2

--- hw3/index.js
+++ hw3/index.js
@@ -1,14 +1,22 @@
-// Создать функцию - конструктор, которая будет иметь внутри все свойства обьекта emplyee из массива emplyeeArr;
+// Задание 1. Создать функцию - конструктор, которая будет иметь внутри все свойства обьекта emplyee из массива emplyeeArr;
 
-const fEmployee = emplyeeArr[1];
+const employee = emplyeeArr[6];
 
 const Employee = function(obj){
       Object.assign(this, obj)
+
+      this.getFullName = function() {        
+        return `${this.name} ${this.surname}`;
+        }
     }
 
-const instance = new Employee(fEmployee)
+const employeeObj = new Employee(employee)
 
-console.log(instance);
+employeeObj.getFullName()
 
+console.log(employeeObj);
 
+console.log(employeeObj.getFullName());
+
+// Задание 2. Добавить функции - конструктору метод (помним про prototype): getFullName который вернет полное имя начиная с фамилии в виде строки
 
